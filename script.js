65baef27d19691dd698562e145e6e09b4f632838
Micah Brown <wss7uv@virginia.edu>
2024-04-02 16:30:29 -0400

    button

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function onButtonClick() {
 
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
+
+const newButton = document.createElement('button');
+newButton.textContent = 'Click this to see a GOAT';
+document.body.appendChild(newButton);
+
+newButton.addEventListener('click', () => {
+    const image = document.createElement('img');
+    image.src = 'IMG_0012.jpeg';
+    document.body.appendChild(image);
+});
