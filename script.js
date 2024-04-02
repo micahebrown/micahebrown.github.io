ce64748ecf333e9e44662af8fc61cd983154030e
Micah Brown <wss7uv@virginia.edu>
2024-04-02 16:23:06 -0400

    button

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function onButtonClick() {
-    console.log('Button Clicked!')
-    const image = document.createElement('img')
-
-    image.src = 'IMG_1192.jpeg';
-
+    const image = document.createElement('img');
+    image.src = 'IMG_0012.jpeg';
     document.body.appendChild(image);
+}
 
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
-}
