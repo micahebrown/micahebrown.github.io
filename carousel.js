d5a2e73157478cd9837b8c09a316631bca52f15e
Micah Brown <wss7uv@virginia.edu>
2024-04-15 21:52:52 -0400

    carousel

--- carousel.js
+++ carousel.js
@@ -1,49 +1,25 @@
-!(function(d){
-var itemClassName = "carousel__photo";
-    items = d.getElementsByClassName(itemClassName),
-    totalItems =items.length,
-    slide = 0,
-    moving = true;
-function setInitialClasses(){
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector('.carousel');
+    const prevButton = document.querySelector('.carousel__button--prev');
+    const nextButton = document.querySelector('.carousel__button--next');
+    const images = document.querySelectorAll('.carousel__photo');
+    let currentIndex = 0;
 
-    items[totalItems - 1].classList.add("prev");
-    items[0].classList.add("active");
-    items[1].classList.add("next");
-}
-
-function setEventListeners(){
-    var next= d.getElementsByClassName('carousel__button--next')[0],
-    prev = d.getElementsByClassName('carousel__button--prev')[0];
-
-    next.addEventListener('click', moveNext);
-    prev.addEventListener('click', movePrev);
-}
-
-function moveNext() {
-
-    if (!moving) {
-
-        if (slide === (totalItems - 1)){
-            slide = 0;
-        } else {
-            slide++;
-        }
-
-        moveCarouselTo(slide);
-        }
+    function goToSlide(index) {
+        const slideWidth = carousel.clientWidth;
+        carousel.style.transform = `translateX(${-index * slideWidth}px)`;
+        currentIndex = index;
     }
 
-function movePrev() {
-
-    if (!moving) {
-
-        if (slide === 0) {
-            slide = (totalItems - 1);
-        } else {
-            slide--;
+    prevButton.addEventListener('click', function() {
+        if (currentIndex > 0) {
+            goToSlide(currentIndex - 1);
         }
+    });
 
-        moveCarouselTo(slide);
+    nextButton.addEventListener('click', function() {
+        if (currentIndex < images.length - 1) {
+            goToSlide(currentIndex + 1);
         }
-    }
-}(document));
+    });
+});
