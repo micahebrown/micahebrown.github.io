ab7af3d394b2e163612b36c4be0307c9e4919c25
Micah Brown <wss7uv@virginia.edu>
2024-04-16 16:09:50 -0400

    pls

--- carousel.js
+++ carousel.js
@@ -1,86 +1,73 @@
-!(function(d){
-    var itemClassName = "carousel__photo";
-       items = d.getElementsByClassName(itemClassName),
-       totalItems =items.length,
-       slide = 0,
-       moving = true;
-    function setInitialClasses(){
-    
-    
-       items[totalItems - 1].classList.add("prev");
-       items[0].classList.add("active");
-       items[1].classList.add("next");
-    }
-    
-    
-    function setEventListeners(){
-       var next= d.getElementsByClassName('carousel__button--next')[0],
-       prev = d.getElementsByClassName('carousel__button--prev')[0];
-    
-    
-       next.addEventListener('click', moveNext);
-       prev.addEventListener('click', movePrev);
-    }
-    
-    
-    function moveNext() {
-    
-    
-       if (!moving) {
-    
-    
-           if (slide === (totalItems - 1)){
-               slide = 0;
-           } else {
-               slide++;
-           }
-    
-    
-           moveCarouselTo(slide);
-           }
-       }
-    
-    
-    function movePrev() {
-    
-    
-       if (!moving) {
-    
-    
-           if (slide === 0) {
-               slide = (totalItems - 1);
-           } else {
-               slide--;
-           }
-    
-    
-           moveCarouselTo(slide);
-           }
-       }
-    }(document));
+(function(d) {
+    var itemClassName = "carousel__photo",
+        items = d.getElementsByClassName(itemClassName),
+        totalItems = items.length,
+        slide = 0,
+        moving = false;
+
+    function setInitialClasses() {
         
-document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector('.carousel');
-    const prevButton = document.querySelector('.carousel__button--prev');
-    const nextButton = document.querySelector('.carousel__button--next');
-    const images = document.querySelectorAll('.carousel__photo');
-    let currentIndex = 0;
-
-    function goToSlide(index) {
-        const slideWidth = carousel.clientWidth;
-        carousel.style.transform = `translateX(${-index * slideWidth}px)`;
-        currentIndex = index;
+        items[totalItems - 1].classList.add("prev");
+        items[0].classList.add("active");
+        items[1].classList.add("next");
     }
 
-    prevButton.addEventListener('click', function() {
-        if (currentIndex > 0) {
-            goToSlide(currentIndex - 1);
-        }
-    });
+    function setEventListeners() {
+        var next = d.querySelector('.carousel__button--next'),
+            prev = d.querySelector('.carousel__button--prev');
 
-    nextButton.addEventListener('click', function() {
-        if (currentIndex < images.length - 1) {
-            goToSlide(currentIndex + 1);
+        next.addEventListener('click', moveNext);
+        prev.addEventListener('click', movePrev);
+    }
+
+    function moveNext() {
+        if (!moving) {
+            if (slide === (totalItems - 1)) {
+                slide = 0;
+            } else {
+                slide++;
+            }
+            moveCarouselTo(slide);
         }
-    });
-});
+    }
+
+    function movePrev() {
+        if (!moving) {
+            if (slide === 0) {
+                slide = (totalItems - 1);
+            } else {
+                slide--;
+            }
+            moveCarouselTo(slide);
+        }
+    }
+
+    function moveCarouselTo(index) {
+        moving = true; 
+      
+        for (var i = 0; i < totalItems; i++) {
+            items[i].classList.remove("prev", "active", "next");
+        }
+        
+        items[index].classList.add("active");
+        if (index === 0) {
+            items[totalItems - 1].classList.add("prev");
+            items[index + 1].classList.add("next");
+        } else if (index === totalItems - 1) {
+            items[index - 1].classList.add("prev");
+            items[0].classList.add("next");
+        } else {
+            items[index - 1].classList.add("prev");
+            items[index + 1].classList.add("next");
+        }
+        
+        setTimeout(function() {
+            moving = false;
+        }, 500); 
+    }
+
+
+    setInitialClasses();
+    setEventListeners();
+})(document);
+
